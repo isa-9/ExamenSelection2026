e306bef6124b695fc7a56b57b774c737b618f9e6
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2026-01-30 13:11:31 -0500

    q9

--- DataManagement/querry/q9.sql
+++ DataManagement/querry/q9.sql
@@ -1,2 +1,10 @@
 -- Classer en ordre décroissant de note et de nombre de personnage pour les consoles qui ont la meuilleure note moyenne pour les jeux sortis.
+SELECT c.nom, AVG(j.note) as note_moyenne, count(distinct pj.personnageID) as nombre_de_personnages
+FROM Sortie s
+JOIN Jeu j ON j.jeuID = s.jeuid
+JOIN developpeur D on d.developpeurID = j.developpeurID
+JOIN PersonnageJeux pj on pj.jeuID = j.jeuID
+JOIN console c on c.consoleid = s.consoleid
+GROUP by c.nom
+ORDER BY AVG(j.note) DESC , count(distinct pj.personnageID) DESC 
 
